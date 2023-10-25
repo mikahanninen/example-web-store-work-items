11400ee8b5cc7cafe90e7175f40868f6dceb4744
Mika Hänninen <mika.hanninen@gmail.com>
2023-10-25 15:06:51 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -25,7 +25,7 @@ def update_workitem(work_item_id, data):
     workspace_id = os.environ["RC_WORKSPACE_ID"]
     api_key = Vault().get_secret("webstore")
     headers = {"Authorization": f"RC-WSKEY {api_key}"}
-    HTTP.session_less_post(
+    HTTP().session_less_post(
         headers=headers,
         url=f"{API_BASE_URL}/{workspace_id}/work-items/{work_item_id}/payload",
         json=data)
@@ -35,7 +35,7 @@ def retry_workitem(work_item_id):
     api_key = Vault().get_secret("webstore")
     headers = {"Authorization": f"RC-WSKEY {api_key}"}
     data = {"batch_operation": "retry", "work_item_ids": [work_item_id]}
-    HTTP.session_less_post(
+    HTTP().session_less_post(
         headers=headers,
         url=f"{API_BASE_URL}/{workspace_id}/work-items/batch",
         json=data)
